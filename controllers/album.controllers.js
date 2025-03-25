acf3e934eec6354c68f32f86d5fd96f36320d18d
Pedro Rosique <rosiquemurciapedro@gmail.com>
2025-03-25 19:55:56 +0100

    finishing server

--- controllers/album.controllers.js
+++ controllers/album.controllers.js
@@ -188,4 +188,30 @@ const deleteAlbum = (req, res, next) => {
 
 }
 
+const deleteAlbums = (req, res, next) => {
+
+    const { id: albumId } = req.params
+    const { _id: author } = req.payload
+
+
+
+    if (!mongoose.Types.ObjectId.isValid(albumId)) {
+        res.status(404).json({ message: 'This id is not valid' })
+    }
+
+    Album
+        .findByIdAndDelete(albumId)
+        .then(deletedAlbum => {
+
+            if (!deletedAlbum) {
+                return res.status(404).json({ message: 'Album not found' });
+            }
+
+            res.sendStatus(200)
+
+        })
+        .catch(err => next(err))
+
+}
+
 module.exports = { getAlbums, getLastAlbums, getAlbum, searchAlbum, createAlbum, editAlbum, deleteAlbum, searchArtistsAlbum }
